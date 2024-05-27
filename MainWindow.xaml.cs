caa057e30c6ba11ad5f7c401cb2f7ee468d81084
Mateo <mateo.santarsiero@gmail.com>
2024-05-27 00:38:18 -0300

    Finalizado

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
@@ -60,6 +61,85 @@ namespace AgendaDeContactos
         {
             string jsonString = JsonConvert.SerializeObject(Contacts,Formatting.Indented);
             File.WriteAllText("contacts.json", jsonString);
+            MessageBox.Show("Guardado!");
+        }
+
+        private void importBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*",
+                Title = "Seleccione el archivo de contactos"
+
+            };
+            string filePath = string.Empty;
+
+            // Mostrar el cuadro de diálogo y obtener la ruta del archivo seleccionado
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filePath = openFileDialog.FileName;
+            }
+            else
+            {
+                Console.WriteLine("No file selected.");
+            }
+
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                ObservableCollection<Contact> contacts = JsonConvert.DeserializeObject<ObservableCollection<Contact>>(json);
+                contactList.ItemsSource = contacts;
+                MessageBox.Show("Importado!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error deserializing file: {ex.Message}");
+            }
+        }
+
+        private void searchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (searchTxT.Text != "" && contactList.Items.Count != 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                switch (searchComboBox.Text)
+                {
+                    case "Nombre":
+                        foreach (Contact c in contactList.ItemsSource)
+                        {
+                            if (searchTxT.Text == c.FirstName)
+                            {
+                                sb.Append(c.ToString()+"\n");
+                            }
+                        }
+                        break;
+                    case "Apellido":
+                        foreach (Contact c in contactList.ItemsSource)
+                        {
+                            if (searchTxT.Text == c.LastName)
+                            {
+                                sb.Append(c.ToString() + "\n");
+                            }
+                        }
+                        break;
+                    case "Teléfono":
+                        foreach (Contact c in contactList.ItemsSource)
+                        {
+                            if (searchTxT.Text == c.PhoneNumber)
+                            {
+                                sb.Append(c.ToString() + "\n");
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                MessageBox.Show(sb.ToString());
+            }
+            else
+            {
+                MessageBox.Show("No hay contactos");
+            }
         }
     }
     public class Contact(string firstName, string lastName, string phoneNumber, string email)
